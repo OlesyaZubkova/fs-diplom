db921a9edca5ceb7f7fd392b64104ce53742d926
Olesya Zubkova <olesya_2802@mail.ru>
2023-03-12 22:39:57 +0300

    services added

--- app/Services/SeatService.php
+++ app/Services/SeatService.php
@@ -4,11 +4,16 @@ namespace App\Services;
 
 use App\Models\Seat;
 use App\Models\Ticket;
+use PhpParser\Error;
 
 class SeatService
 {
     public function create(array $params): Seat
     {
+
+        if (!$params['seats']) {
+            throw new Error("Error! Cannot create tickets for seats");
+        }
 
         foreach ($params['seats'] as $seatId) {
             $seat = Seat::findOrFail($seatId);
